665e50b493d8f6e2a76b09cf0b87ac7944908aa0
Kailan Blanks | Komp <kailan@komputerking.net>
2014-08-21 02:10:50 +0100

    Initial commit.

---
+++ src/main/java/net/komputerking/tracker/Tracker.java
@@ -0,0 +1,12 @@
+package net.komputerking.tracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tracker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
