17ba2bd146b2d1efd1bb22cd93cb9e96e5f8e6b5
Kailan Blanks | Komp <kailan@komputerking.net>
2014-08-21 02:56:32 +0100

    Add event to analyze last damage on death.

--- src/main/java/net/komputerking/tracker/Tracker.java
+++ src/main/java/net/komputerking/tracker/Tracker.java
@@ -1,5 +1,6 @@
 package net.komputerking.tracker;
 
+import net.komputerking.tracker.api.Damage;
 import net.komputerking.tracker.events.PlayerDamageEvent;
 import net.komputerking.tracker.trackers.PVPTracker;
 import net.komputerking.tracker.util.UnknownDamage;
@@ -8,8 +9,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Tracker extends JavaPlugin implements Listener {
@@ -55,6 +58,22 @@ public class Tracker extends JavaPlugin implements Listener {
                 event.setCancelled(true);
             } else {
                 recordHandler.addDamage(e.getTrackerDamage());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        List<Damage> record = recordHandler.getRecord(event.getEntity());
+        if (record == null) {
+            Bukkit.broadcastMessage("record is empty");
+        } else {
+            Damage deathCause = record.get(record.size()-1);
+            if (deathCause instanceof UnknownDamage) {
+                Bukkit.broadcastMessage("we don't know how they died");
+            } else if (deathCause instanceof PVPTracker.PVPDamage) {
+                PVPTracker.PVPDamage d = (PVPTracker.PVPDamage) deathCause;
+                Bukkit.broadcastMessage("they were killed by " + d.getDamager().getName() + " and the last hit took " + d.getDamage() + " health");
             }
         }
     }
